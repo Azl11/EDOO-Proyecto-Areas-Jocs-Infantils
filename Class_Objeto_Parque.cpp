1ecb59c30e5499fa9a4c32b42b53125cd53b2baa
Azl11 <109969558+Azl11@users.noreply.github.com>
2024-03-09 11:19:15 +0000

    Update Class_Objeto_Parque.cpp

--- Class_Objeto_Parque.cpp
+++ Class_Objeto_Parque.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 //Class_1
-class ObjetoParque{
-private:
-int AJ_Id , AJ_M2 , AJ_Edat_Id; //Area de Joc Id , Area de Joc M2 , Area de Joc Edat Id
-string AJ_Edat; //Area de Joc Edat
-string Codi , Tipus; //Codi , Tipus d'objecte 
-public:
+class ObjetoParque
+{
+  private:
+  int AJ_Id , AJ_M2 , AJ_Edat_Id; //Area de Joc Id , Area de Joc M2 , Area de Joc Edat Id
+  string AJ_Edat; //Area de Joc Edat
+  string Codi , Tipus; //Codi , Tipus d'objecte 
+  public:
+};
