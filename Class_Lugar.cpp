ec47448c6e2429789c429bc30389b3992c3cf617
Azl11 <109969558+Azl11@users.noreply.github.com>
2024-03-09 11:19:32 +0000

    Update Class_Lugar.cpp

--- Class_Lugar.cpp
+++ Class_Lugar.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 //Class_2
-class Lugar{
-private:
-int Codi_V , Codi_D , Codi_B; //Codi Via , Codi Districte , Codi Barri
-string Nom_V , Nom_D , Nom_B; //Nom Via , Nom Districte , Nom Barri
-char Tipus_V; //Tipus Via 
-int Num_P; //Numero Postal
-double Long , Lat; //Longitud  Latitud
-public:
+class Lugar
+{
+  private:
+  int Codi_V , Codi_D , Codi_B; //Codi Via , Codi Districte , Codi Barri
+  string Nom_V , Nom_D , Nom_B; //Nom Via , Nom Districte , Nom Barri
+  char Tipus_V; //Tipus Via 
+  int Num_P; //Numero Postal
+  double Long , Lat; //Longitud  Latitud
+  public:
+};
