08cd5f918c08c527b60269a9b2fc5695fb990fe5
Azl11 <109969558+Azl11@users.noreply.github.com>
2024-03-18 11:26:49 +0000

    Update Class_Objeto_Parque.cpp

--- Class_Objeto_Parque.cpp
+++ Class_Objeto_Parque.cpp
@@ -7,7 +7,8 @@ class ObjetoParque
 {
     private:
 
-    int AJ_Id , AJ_M2 , AJ_Edat_Id; //Area de Joc Id , Area de Joc M2 , Area de Joc Edat Id
+    int AJ_Id , AJ_Edat_Id; //Area de Joc Id , Area de Joc M2 , Area de Joc Edat Id
+    double AJ_M2 ;
     string AJ_Edat; //Area de Joc Edat
     string Codi , Tipus; //Codi , Tipus d'objecte
 
@@ -15,17 +16,9 @@ class ObjetoParque
 
 //-------CONSTRUCTORS--------
 
-    ObjetoParque()
-    {
-    AJ_Id = 0;
-    AJ_M2 = 0;
-    AJ_Edat_Id = 0;
-    AJ_Edat = "-";
-    Codi = "-";
-    Tipus = "-";
-    }
+    ObjetoParque(){}
 
-    ObjetoParque(int C_AJ_Id , int C_AJ_M2 , int C_AJ_Edat_Id, string C_AJ_Edat , string C_Codi , string C_Tipus)
+    ObjetoParque(int C_AJ_Id , double C_AJ_M2 , int C_AJ_Edat_Id, string C_AJ_Edat , string C_Codi , string C_Tipus)
     {
     AJ_Id = C_AJ_Id;
     AJ_M2 = C_AJ_M2;
@@ -52,7 +45,7 @@ class ObjetoParque
 //-----------Get-------------
 
     int getAJ_Id()const{return AJ_Id;}
-    int getAJ_M2()const{return AJ_M2;}
+    double getAJ_M2()const{return AJ_M2;}
     int getAJ_Edat_Id()const{return AJ_Edat_Id;}
     string getAJ_Edat()const{return AJ_Edat;}
     string getCodi()const{return Codi;}
@@ -61,7 +54,7 @@ class ObjetoParque
 //----------Set--------------
 
     void setAJ_Id(int S_AJ_Id){AJ_Id = S_AJ_Id;}
-    void setAJ_M2(int S_AJ_M2){AJ_M2 = S_AJ_M2;}
+    void setAJ_M2(double S_AJ_M2){AJ_M2 = S_AJ_M2;}
     void setAJ_Edat_Id(int S_AJ_Edat_Id){AJ_Edat_Id = S_AJ_Edat_Id;}
     void setAJ_Edat(string S_AJ_Edat){AJ_Edat = S_AJ_Edat;}
     void setCodi(string S_Codi){Codi = S_Codi;}
