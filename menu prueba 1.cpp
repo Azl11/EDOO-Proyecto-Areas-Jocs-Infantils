475b3bb1dd9eb783af2170cff925b2676be2a46f
yo <153465573+RanaMagika@users.noreply.github.com>
2024-03-21 16:49:07 +0100

    Update menu prueba 1.cpp

--- menu prueba 1.cpp
+++ menu prueba 1.cpp
@@ -1,16 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
 void MENU(){
 cout<<"|-------------------------------------------------------------------------|"<<endl;
 cout<<"Escull una opcio:\n" <<endl;
 cout<<"(1): Consultar informacio parc \n"<<endl;
 cout<<"(2):Modificar Informacio parc \n"<<endl;
 cout<<"(3):Eliminar Parc/Objecte \n"<<endl;
-cout<<"(4):Afegir Parc/Objecte \n"<<endl;
+cout<<"(4):Afegir Parc/Objecte \n"<<endl; 
 cout<<"(5):Comparar Parcs \n"<<endl;
 cout<<"(6):Identificar Parcs en zona\n"<<endl;
 cout<<"|-------------------------------------------------------------------------|"<<endl;    
 }
-void selecM1(int OP){
 
+void Repetir(int OPb){
+
+cin>>OPb;
+selecM1(int OPb);
+}
+
+void selecM1(int OP){
+switch (OP){
+case 1: cout<<"Consulta informacio de parc escollida! "<<endl;
+    //aqui iria un subprograma para esta opcion
+    break;
+case 2: cout<<"Modificar Informacio parc escollida! "<<endl;
+     //aqui iria un subprograma para esta opcion
+    break;
+case 3: cout<<" Eliminar Parc/Objecte escollida! "<<endl;
+     //aqui iria un subprograma para esta opcion
+    break;
+case 4: cout<<"Afegir Parc/Objecte escollida! "<<endl;
+     //aqui iria un subprograma para esta opcion
+    break;
+case 5: cout<< " Comparar parcs escollida! "<<endl;
+     //aqui iria un subprograma para esta opcion
+    break;
+case 6: cout<<" Identificar Parcs en zona escollida! "<<endl;
+     //aqui iria un subprograma para esta opcion
+    break;
+    default:
+      cout<<"Elije una opcion del menu!!! "<<endl;
+        Repetir(OPb);
+        break;
+}
 
 
 
@@ -19,7 +53,7 @@ void selecM1(int OP){
 int main(){
 int opcion;
 MENU();
-
+selecM1(opcion);
 
 
 
