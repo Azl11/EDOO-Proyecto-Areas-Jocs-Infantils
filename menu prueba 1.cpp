c7cb345d504f6d9c496ed1bb8059a55900914dd7
Azl11 <109969558+Azl11@users.noreply.github.com>
2024-05-11 21:04:04 +0000

    Update menu prueba 1.cpp

--- menu prueba 1.cpp
+++ menu prueba 1.cpp
@@ -1,69 +1,79 @@
-#include <iostream>
-using namespace std;
+int main()
+{
+    system("color E0");
 
+    bool t = true, y = true;
+    int x;
 
-void MENU(){
-cout<<"|-------------------------------------------------------------------------|"<<endl;
-cout<<"Escull una opcio:\n" <<endl;
-cout<<"(1): Consultar informacio parc \n"<<endl;
-cout<<"(2):Modificar Informacio parc \n"<<endl;
-cout<<"(3):Eliminar Parc/Objecte \n"<<endl;
-cout<<"(4):Afegir Parc/Objecte \n"<<endl;
-cout<<"(5):Comparar Parcs \n"<<endl;
-cout<<"(6):Identificar Parcs en zona\n"<<endl;
-cout<<"|-------------------------------------------------------------------------|"<<endl;
+    ifstream Data("DATA.txt");
+    Parques_Barna P(Data);
+
+    while(t)
+    {
+        cout<<endl;
+        cout<<" |-------------------------------------------------------------------------|"<<endl;
+        cout<<" Escull una opcio valida:\n" <<endl;
+        cout<<" (1): Consultar informacio parc \n"<<endl;
+        cout<<" (2):Modificar Informacio parc \n"<<endl;
+        cout<<" (3):Eliminar Parc/Objecte \n"<<endl;
+        cout<<" (4):Afegir Parc/Objecte \n"<<endl;
+        cout<<" (5):Sortir del programa \n"<<endl;
+        cout<<" |-------------------------------------------------------------------------|"<<endl<<endl;
+        cout<<" ";cin>> x;
+        switch (x)
+        {
+        case 1:
+            while(y){
+                system("cls");
+                cout<<endl;
+                cout<<" |-------------------------------------------------------------------------|"<<endl;
+                cout<<" Escull una opcio valida:\n" <<endl;
+                cout<<" (1):Busqueda per ID \n"<<endl;
+                cout<<" (2):Busqueda per Nom del Barri \n"<<endl;
+                cout<<" (3):Busqueda per Nom del Districte \n"<<endl;
+                cout<<" (4):Busqueda per Codi Postal \n"<<endl;
+                cout<<" (5):Busqueda per Edat \n"<<endl;
+                cout<<" (6):Tornar enrera \n"<<endl;
+                cout<<" |-------------------------------------------------------------------------|"<<endl<<endl;
+                cout<<" ";cin>> x;
+                switch(x)
+                {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    system("cls");
+                    main();
+                }
+            }
+
+            break;
+        case 2:
+            P.Edit();
+            break;
+        case 3:
+            P.Delete();
+            break;
+        case 4:
+            P.Add();
+            break;
+        case 5:
+            return 0;
+        default:
+            main();
+            break;
+        }
+    }
 }
-
-
-
-void selecM1(int OP);
-void Repetir(int OPb);
-
-
-
-
-
-
-int main(){
-int opcion;
-MENU();
-cin>>opcion;
-selecM1(opcion);
-
-
-
-
-}
-
-void selecM1(int OP){
-switch (OP){
-case 1: cout<<"Consulta informacio de parc escollida! "<<endl;
-    //aqui iria un subprograma para esta opcion
-    break;
-case 2: cout<<"Modificar Informacio parc escollida! "<<endl;
-     //aqui iria un subprograma para esta opcion
-    break;
-case 3: cout<<" Eliminar Parc/Objecte escollida! "<<endl;
-     //aqui iria un subprograma para esta opcion
-    break;
-case 4: cout<<"Afegir Parc/Objecte escollida! "<<endl;
-     //aqui iria un subprograma para esta opcion
-    break;
-case 5: cout<< " Comparar parcs escollida! "<<endl;
-     //aqui iria un subprograma para esta opcion
-    break;
-case 6: cout<<" Identificar Parcs en zona escollida! "<<endl;
-     //aqui iria un subprograma para esta opcion
-    break;
-    default:
-      cout<<"Elije una opcion del menu!!! "<<endl;
-        Repetir(OP);
-        break;
-}
-}
-void Repetir(int OPb){
-
-cin>>OPb;
-selecM1(OPb);
-}
-
